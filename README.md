2ac3a5ea6e7cdf2b2474af116bfb878196897be1
carloscha82 <carloscha82@hotmail.com>
2021-12-28 20:52:14 -0500

    Se crea funciones del calculo de area y perimetro del circulo, triangulo y cuadrado

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # javascript-practico
 Curso de platzi
+
+## Taller #1: Figuras geometricas
+
+- Primer paso: definir las formulas
+- Segundo paso: implementar las formulas en javascript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar javascript con html
---
+++ figuras.js
@@ -0,0 +1,25 @@
+// Codigo del cuadrado
+
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden (cm): " + ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es (cm): " + perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado ** 2;
+console.log("El area del cuadrado es (cm^2): " + areaCuadrado);
+
+
+// Codigo del triengulo
+
+const lado1Triangulo = 6;
+const lado2Triangulo = 6;
+const baseTriangulo = 4;
+
+console.log("Los lados del triangulo miden (cm): " + ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es (cm): " + perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado ** 2;
+console.log("El area del cuadrado es (cm^2): " + areaCuadrado);
